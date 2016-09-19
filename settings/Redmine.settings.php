0a8133cb2a3d5e39179ad2425023bdd561069941
hans <hidink@orgis.nl>
2016-09-19 21:17:33 +0200

    Add settings

---
+++ settings/Redmine.settings.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Settings metadata file
+ */
+return array (
+    'redmine_api_key' => array(
+        'group_name' => 'Redmine Preferences',
+        'group' => 'civiredmine',
+        'name' => 'redmine_api_key',
+        'type' => 'String',
+        'add' => '4.6',
+        'is_domain' => 1,
+        'is_contact' => 0,
+        'title' => 'Redmine user API Key',
+        'description' => 'The user API Key for communicating with Mailchimp.',
+        'help_text' => '',
+        'html_type' => 'Text',
+        'html_attributes' => array(
+            'size' => 100,
+        ),
+        'quick_form_type' => 'Element',
+    ),
+    'mailchimp_webhook_base_url' => array(
+        'group_name' => 'Redmine Preferences',
+        'group' => 'civiredmine',
+        'name' => 'redmine_base_url',
+        'type' => 'String',
+        'add' => '4.6',
+        'is_domain' => 1,
+        'is_contact' => 0,
+        'title' => 'Redmine Base URL',
+        'description' => 'The Redmine Base URL',
+        'help_text' => '',
+        'html_type' => 'Text',
+        'html_attributes' => array(
+            'size' => 100,
+        ),
+        'quick_form_type' => 'Element',
+    ),
+);
